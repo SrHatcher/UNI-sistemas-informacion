fb7375a1053e680fe12720c0879f8c6da75f5d7d
SrHatcher <erickomaldonado11@gmail.com>
2022-10-12 17:34:16 -0600

    clase de tags y clase app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config'
-import express from 'express';
+import app from './app'
+//import express from 'express';
+import App from './app'
 
-const PORT= process.env.APP_PORT || 3000;
+//const PORT= process.env.APP_PORT || 3000;
+//const app = express();
 
-const app = express();
+//app.listen(PORT, ()=> {
+//    console.log(`server working in port ${PORT}`)
+//})
 
-app.listen(PORT, ()=> {
-    console.log(`server working in port ${PORT}`)
-})
+App.listen(process.env.APP_PORT as unknown as number || 3000)
